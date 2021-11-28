5ebed3f42b62176fc878e38dbdda7f8c48a77c77
qvabrik <k.zhilkov@gmail.com>
2021-11-28 19:05:14 +0300

    Change yes/no to y/n

--- main.py
+++ main.py
@@ -13,16 +13,16 @@ print('App has been loaded...')
 userName = input('Type your name: ')
 Hello(userName)
 
-command = "yes"
-while command == "yes":
+command = "y"
+while command == "y":
     someNumb = r.numberInput()
     newNumb = r.randNumber(someNumb)
     print('Number ' + str(someNumb) + ' has became ' + str(newNumb) + '.')
     db.DBplus(someNumb, newNumb)
     print('Input-output has been added to database.')
     while True:
-        command = input('Change another number? [yes]/[no]: ')
-        if command == "yes" or command == "no":
+        command = input('Try another number? [y]/[n]: ')
+        if command == "y" or command == "n":
             break
         else:
             print('Unknown command. Please try again.')
