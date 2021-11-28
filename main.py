b75f8460346c9cdd08f7d46c57ba0ee619937690
qvabrik <k.zhilkov@gmail.com>
2021-11-28 18:43:03 +0300

    Menu added

--- main.py
+++ main.py
@@ -13,8 +13,17 @@ print('App has been loaded...')
 userName = input('Type your name: ')
 Hello(userName)
 
-someNumb = r.numberInput()
-newNumb = r.randNumber(someNumb)
-print('Number ' + str(someNumb) + ' has became ' + str(newNumb) + '.')
-
-db.DBplus(someNumb, newNumb)
+command = "yes"
+while command == "yes":
+    someNumb = r.numberInput()
+    newNumb = r.randNumber(someNumb)
+    print('Number ' + str(someNumb) + ' has became ' + str(newNumb) + '.')
+    db.DBplus(someNumb, newNumb)
+    print('Input-output has been added to database.')
+    while True:
+        command = input('Change another number? [yes]/[no]: ')
+        if command == "yes" or command == "no":
+            break
+        else:
+            print('Unknown command. Please try again.')
+            continue
